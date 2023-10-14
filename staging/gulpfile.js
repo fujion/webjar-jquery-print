30134fd432293b667e1e3862e9bddf1726d4fe62
Douglas Martin <mdgeek1@gmail.com>
2023-10-13 20:55:22 -0400

    Use gulp and rollup for builds.

---
+++ staging/gulpfile.js
@@ -0,0 +1,27 @@
+const {src, dest, series} = require('gulp');
+
+const srcDir = '${src.rootdir}/jQuery.print-${version.unrevise}/';
+const destDir = '${webjar.target}/';
+
+function task1() {
+    return _copy(['LICENSE', '*.md']);
+}
+
+function task2() {
+    return _copy(['jQuery.print.js', 'dist/jQuery.print.min.js'], 'dist');
+}
+
+function _toSrc(_src) {
+    return src(_src, {allowEmpty: false, cwd: srcDir});
+}
+
+function _toDest(_dest) {
+    return dest(_dest || '.', {cwd: destDir});
+}
+
+function _copy(_src, _dest) {
+    console.log('  Copying ' + _src);
+    return _toSrc(_src).pipe(_toDest(_dest))
+}
+
+exports.default = series(task1, task2);
